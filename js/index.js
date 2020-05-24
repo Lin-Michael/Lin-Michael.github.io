3ba667799843c6400dbd4a2371ba36ccc3270222
mike <38706072+MichaelJonathanLin@users.noreply.github.com>
2020-05-24 13:16:44 -0400

    Added Dark mode theme

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
+var light = "#fff";
+var dark = "#303030";
+
+//light: fff
+//dark: #303030
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
@@ -10,3 +15,28 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+function resetButton(){
+    document.getElementById("darkMode").checked = false;
+}
+
+function darkMode(checkbox){
+
+    if(checkbox.checked)//dark theme
+    {
+        var x = document.getElementsByTagName("BODY")[0];
+        x.style.background = dark;
+        x.style.color = light;
+        var y = document.getElementsByClassName("resume");
+        y[0].style.color = light;
+        y[1].style.color = light;
+    }
+    else{
+        var x = document.getElementsByTagName("BODY")[0];
+        x.style.background = "#fff";
+        x.style.color = dark;
+        var y = document.getElementsByClassName("resume");
+        y[0].style.color = dark;
+        y[1].style.color = dark;
+    }
+
+}
