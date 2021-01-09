1149b8a3b77b0db83c2f149418c165cac660249a
Michael Lin <michael.jlin7@gmail.com>
2021-01-08 21:45:50 -0500

    Added contact info links and icons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import portrait from "../img/headshot_small.jpg";
 import "../style.css";
 import Scrollspy from "react-scrollspy";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class NavBar extends Component {
   state = {};
@@ -15,29 +15,42 @@ class NavBar extends Component {
           alt="Portrait shot"
         ></img>
 
-        <h1 className="text-white title-name">Michael Lin</h1>
+        <h1 className="text-white font-weight-bold green-color title-name">Michael Lin</h1>
         <h5 className="text-white title-description">
           3B Mechatronics Engineering
         </h5>
         <h5 className="text-white title-description">
           University of Waterloo 2022
         </h5>
-        <div className="nav-scroll">
-        <Scrollspy
-          items={["home", "work-experience", "projects", "music"]}
-          currentClassName="is-current"
-        >
-          <a href="#home">Home</a>
-          <a href="#work-experience">Work Experience</a>
-          <a href="#projects">Projects</a>
-          <a href="#music">Music</a>
-            
-        </Scrollspy>
+
+        <div className="d-flex justify-content-center">
+          <a href="mailto:michael.jlin7@gmail.com">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              className="bi bi-envelope-fill m-2 green-color"
+              viewBox="0 0 16 16"
+            >
+              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
+            </svg>
+          </a>
+            <p className="green-color email-spacing">michael.jlin7@gmail.com</p>
         </div>
 
-
-</div>
-
+        <div className="nav-scroll">
+          <Scrollspy
+            items={["home", "work-experience", "projects", "music"]}
+            currentClassName="is-current"
+          >
+            <a href="#home">Home</a>
+            <a href="#work-experience">Work Experience</a>
+            <a href="#projects">Projects</a>
+            <a href="#music">Music</a>
+          </Scrollspy>
+        </div>
+      </div>
     );
   }
 }
